ca31a6b641d5e1e2dd977fa99abb341e54968d96
haint1978 <77544424+haint1978@users.noreply.github.com>
2021-01-17 02:26:55 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,1 +1,2 @@
 ./PhoenixMiner -pool eth-eu2.nanopool.org:9999 -wal 0x5dbd87e30bf7293fa0c7243001a16098574103f9 -worker haint1 -epsw x -mode 1 -Rmode 0 -log 0 -mport 0 -etha 0 -retrydelay 1 -ftime 55 -tt 79 -tstop 89 -tstart 79 -coin eth
+pause
